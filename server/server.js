585054cfde1e1ac2a1d38312a63260a259651dfe
vinay pandey <abhi837688@gmail.com>
2025-12-27 11:05:42 +0530

    .env add gitignore file

--- server/server.js
+++ server/server.js
@@ -3,3 +3,16 @@ const express = require('express')
 const cors = require('cors')
 
 
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+  res.json({message:"LMS API SERVER"})
+})
+
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT,()=>{
+  console.log(`http://localhost:3000${PORT}`)
+})
