5bb1af20c50b27b8bf2271fb254edef3ca1474e2
vinaypandey <abhi837688@gmail.com>
2026-02-02 19:41:03 +0530

    project complete

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,7 +3,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import store from './app/store'
+import store from './app/store.js'
 import './index.css'
 import App from './App.jsx'
 
